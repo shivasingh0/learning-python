30f5e05f09773acdb1da00e96db8c47f39bee48a
shivasingh0 <shiva2003rajawat@gmail.com>
2024-03-18 01:31:34 +0530

    first commit

---
+++ lecture1.py
@@ -0,0 +1,20 @@
+print("hello")
+print(5)
+
+# Day 6
+# escape sequence character
+# \n, \"\"
+print("hello \"mr. raja\" \nyou are not a good man")
+
+print('shibbu', 'singh', sep="|", end=" Rajawat \n")
+
+a = 10
+b = 15
+print(a * b)
+
+# condition
+p = 9 
+if(p > 5) :
+    print('Yes')
+else:
+    print('no')
---
+++ lecture2.py
@@ -0,0 +1,8 @@
+r = 10
+area = 3.14*r*r
+print(area)
+
+# Take user input
+radius = float(input("Enter radius"))  # Need to use float or int when you want to number input from user
+ar = 3.14*radius*radius
+print(ar)
